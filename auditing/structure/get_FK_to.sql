1b59b20e68f3bb2c6ee49af0bfa6fc5385d9c26d
Matthew Jackson <6383741+mwj8410@users.noreply.github.com>
2018-10-02 08:28:23 -0500

    feat(structure) add FK to table query

---
+++ auditing/structure/get_FK_to.sql
@@ -0,0 +1,15 @@
+-- Retrieves all FK constraints that reference a specific table
+
+SELECT
+  table_constraints.constraint_name AS constraint_name,
+  table_constraints.table_schema AS table_schema,
+  table_constraints.table_name AS table_name,
+  key_column.column_name AS column_name
+FROM information_schema.table_constraints AS table_constraints
+JOIN information_schema.key_column_usage AS key_column
+  ON table_constraints.constraint_name = key_column.constraint_name
+WHERE
+  constraint_type = 'FOREIGN KEY'
+  AND
+  table_constraints.table_name = '<table name>'
+;
